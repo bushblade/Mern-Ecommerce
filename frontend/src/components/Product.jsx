9798452ed1be83c59175656bcca5d65aad625419
Will Adams <bushblade@gmail.com>
2021-03-17 08:18:26 +0000

    lesson 7 Rating Component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 function Product({ product }) {
   return (
@@ -16,9 +17,10 @@ function Product({ product }) {
           </Card.Title>
         </a>
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
