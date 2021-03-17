0786de2bbdc657b96dfbb10a1a984d50ee8f9999
Will Adams <bushblade@gmail.com>
2021-03-17 08:49:04 +0000

    lesson 8 react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
