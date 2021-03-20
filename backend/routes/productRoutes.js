74eb234e36b7ada719faedabc262c4b3a369e91d
Will Adams <bushblade@gmail.com>
2021-03-20 07:49:20 +0000

    express 5 remove asyncHandler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 import Product from '../models/ProductModel.js'
 
 const router = express.Router()
@@ -7,27 +6,21 @@ const router = express.Router()
 // @desc            Fetch all products
 // @route           GET /api/products
 // @access          Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
+router.get('/', async (req, res) => {
+  const products = await Product.find({})
+  return res.json(products)
+})
 
 // @desc            Fetch single product by id
 // @route           GET /api/products/:id
 // @access          Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      return res.json(product)
-    } else {
-      return res.status(404).json({ message: 'Product not found' })
-    }
-  })
-)
+router.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    return res.json(product)
+  } else {
+    return res.status(404).json({ message: 'Product not found' })
+  }
+})
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ const app = express()
 
 app.use('/api/products', productRoutes)
 
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
