ccb5fcfcfb5e426681d9318bc07ce1af59206908
Will Adams <bushblade@gmail.com>
2021-03-22 16:54:24 +0000

    remove duplicate rating

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -44,12 +44,6 @@ function ProductScreen({ match }) {
                   text={`${product.numReviews} reviews`}
                 />
               </ListGroup.Item>
-              <ListGroup.Item>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                />
-              </ListGroup.Item>
               <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
               <ListGroup.Item>
                 Description: ${product.description}
