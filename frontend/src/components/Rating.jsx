3574b9341abad03036bb087e916fd9b66ddee6cc
Will Adams <bushblade@gmail.com>
2021-03-22 16:53:25 +0000

    refactore rating

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -4,76 +4,22 @@ import PropTypes from 'prop-types'
 function Rating({ text, value, color }) {
   return (
     <div className='rating'>
-      <span>
-        <i
-          style={{
-            color,
-          }}
-          className={
-            value >= 1
-              ? 'fas fa-star'
-              : value >= 0.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{
-            color,
-          }}
-          className={
-            value >= 2
-              ? 'fas fa-star'
-              : value >= 1.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{
-            color,
-          }}
-          className={
-            value >= 3
-              ? 'fas fa-star'
-              : value >= 2.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{
-            color,
-          }}
-          className={
-            value >= 4
-              ? 'fas fa-star'
-              : value >= 3.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{
-            color,
-          }}
-          className={
-            value >= 5
-              ? 'fas fa-star'
-              : value >= 4.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
+      {Array.from({ length: 5 }).map((_, i) => (
+        <span key={i}>
+          <i
+            style={{
+              color,
+            }}
+            className={
+              value >= i + 1
+                ? 'fas fa-star'
+                : value >= i + 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        </span>
+      ))}
       <span>{text ? text : null}</span>
     </div>
   )
