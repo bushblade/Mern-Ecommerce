669bdcbbb0482151f97cfabd9c60436f3a515c73
Will Adams <bushblade@gmail.com>
2021-03-22 17:03:47 +0000

    small refactor

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types'
 function Rating({ text, value, color }) {
   return (
     <div className='rating'>
-      {Array.from({ length: 5 }).map((_, i) => (
-        <span key={i}>
+      {[1, 2, 3, 4, 5].map((n) => (
+        <span key={n}>
           <i
             style={{
               color,
             }}
             className={
-              value >= i + 1
+              value >= n
                 ? 'fas fa-star'
-                : value >= i + 0.5
+                : value >= n - 0.5
                 ? 'fas fa-star-half-alt'
                 : 'far fa-star'
             }
